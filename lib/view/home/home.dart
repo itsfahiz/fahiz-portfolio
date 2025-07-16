4865f6adb0fdbb53bbbcdb4081c15c96b81b4ec3
itsfahiz <itsfahizp@gmail.com>
2025-07-16 11:54:34 +0400

    Initial commit: basic Flutter Web project

---
+++ lib/view/home/home.dart
@@ -0,0 +1,16 @@
+import 'package:fahiz_portfolio/view/certifications/certifications.dart';
+import 'package:fahiz_portfolio/view/intro/introduction.dart';
+import 'package:fahiz_portfolio/view/main/main_view.dart';
+import 'package:fahiz_portfolio/view/projects/project_view.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainView(
+      pages: [const Introduction(), ProjectsView(), Certifications()],
+    );
+  }
+}
