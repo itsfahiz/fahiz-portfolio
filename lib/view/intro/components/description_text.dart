6c0756ef77dbeb1cb725313b71746f2ed3c5fabd
itsfahiz <itsfahizp@gmail.com>
2025-08-08 15:10:35 +0400

    Refactor AnimatedDescriptionText to improve responsiveness and enhance text display

--- lib/view/intro/components/description_text.dart
+++ lib/view/intro/components/description_text.dart
@@ -10,18 +10,29 @@ class AnimatedDescriptionText extends StatelessWidget {
   });
   final double start;
   final double end;
+
   @override
   Widget build(BuildContext context) {
-    return TweenAnimationBuilder(
+    final isMobile = Responsive.isMobile(context);
+
+    final firebaseAiText =
+        isMobile
+            ? 'Firebase & AI-driven \nstacks.'
+            : 'Firebase & AI-driven stacks.';
+
+    final text =
+        'I build intelligent cross-platform apps from idea'
+        '${isMobile ? '\n' : ' '}to deployment using Flutter'
+        '${isMobile ? ',' : ',\n'}'
+        '$firebaseAiText';
+
+    return TweenAnimationBuilder<double>(
       tween: Tween(begin: start, end: end),
       duration: const Duration(milliseconds: 200),
       builder: (context, value, child) {
         return Text(
-          'I build intelligent cross-platform apps from idea'
-          '${Responsive.isLargeMobile(context) ? '\n' : ' '}to deployment using Flutter'
-          '${Responsive.isLargeMobile(context) ? ',' : ',\n'}'
-          'Firebase & AI-driven stacks.',
-          maxLines: 2,
+          text,
+          maxLines: 3,
           overflow: TextOverflow.ellipsis,
           style: TextStyle(color: Colors.grey, wordSpacing: 2, fontSize: value),
         );
